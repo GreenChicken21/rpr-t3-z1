945b3c62243c6907e915d0dc0d52fa6ea28dbfa2
GreenChicken21 <dcukurija1@etf.unsa.ba>
2018-10-27 20:05:22 +0200

    zapoceta jedna klasa sa par metoda enum Gradovi

---
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -0,0 +1,23 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+enum Grad {TRAVNIK, ORASJE, ZENICA, SARAJEVO, LIVNO, TUZLA, MOSTAR, BIHAC, GORAZDE, SIROKI_BRIJEG, BRCKO };
+public class FiksniBroj extends TelefonskiBroj {
+
+    public Grad grad;
+    public String broj;
+
+    FiksniBroj(Grad grad, String broj){
+
+    }
+
+    public String ispisi(){
+        int niz[]{30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 49};
+        return "0" + niz[grad] + broj;
+    }
+
+    @Override
+    public int hashCode(){
+
+    }
+
+}
