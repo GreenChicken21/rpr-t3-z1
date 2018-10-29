17d1faac9f34b33dbf0e60a28a2f4bee286863c4
GreenChicken21 <dcukurija1@etf.unsa.ba>
2018-10-29 14:21:10 +0100

    napravljena klasa MobilniBroj sa nekoliko metoda metoda

---
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+public class MobilniBroj extends TelefonskiBroj {
+
+    public int mobilnaMreza;
+    public String broj;
+
+    public String ispisi(){
+        return null;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
