338edbe5c35957b8cd9156df47e3778bcfc375a2
GreenChicken21 <dcukurija1@etf.unsa.ba>
2018-10-29 14:26:22 +0100

    napravljen konstruktor za klasu MobilniBroj

--- src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -5,8 +5,12 @@ public class MobilniBroj extends TelefonskiBroj {
     public int mobilnaMreza;
     public String broj;
 
+    public MobilniBroj(int mobilnaMreza, String broj){
+        this.mobilnaMreza = mobilnaMreza;
+        this.broj = broj;
+    }
     public String ispisi(){
-        return null;
+        return "0" + mobilnaMreza + "/" + broj;
     }
 
     @Override
